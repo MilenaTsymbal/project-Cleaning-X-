50c88ad347307c28408b7d3ccff702c13de44c4e
Milena Tsymbal <cymbalale13@gmail.com>
2023-01-30 18:59:59 +0200

    section 3 video

--- main.js
+++ main.js
@@ -1,6 +1,15 @@
-let video_player = document.getElementsById('video-player1');
-let video_about_cleaning = document.getElementsById('video-about-cleaning');
+let video_player = document.getElementById('video-player1');
+let video_about_cleaning = document.getElementById('video-about-cleaning');
+const videoPoster = document.querySelector('.video-poster');
+// const videoPlayerMP4 = document.getElementById('video-player-mp4')
 
-function stopVideo(){
+function stopVideo() {
     video_player.style.display = "none";
+    videoPoster.style.opacity = .5;
+}
+
+function playVideo(file) {
+    video_about_cleaning.src = file;
+    video_player.style.display = "block";
+    videoPoster.style.opacity = 1;
 }
